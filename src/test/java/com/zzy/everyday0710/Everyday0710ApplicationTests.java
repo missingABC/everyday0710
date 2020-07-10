fa2b1bbda95cfb3192833ab5d71bc48c5b14c842
zhengzhongyi <zhongyi0228@sina.cn>
2020-07-10 10:59:26 +0800

    忽略文件，pom和md文件

---
+++ src/test/java/com/zzy/everyday0710/Everyday0710ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzy.everyday0710;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Everyday0710ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
